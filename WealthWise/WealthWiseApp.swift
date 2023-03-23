35cc9699b7d31d2a9b2364f338421e75d6847fce
Joao Gripp <corguinha@gmail.com>
2023-03-23 00:18:39 -0300

    Initial Commit

---
+++ WealthWise/WealthWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WealthWiseApp.swift
+//  WealthWise
+//
+//  Created by Joao Gripp on 23/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WealthWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
